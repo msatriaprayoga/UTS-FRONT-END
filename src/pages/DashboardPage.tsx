f45b1dcffd482aedda26cb08d2cccc21a3744eab
msatriaprayoga <msatriaprayoga92@gmail.com>
2026-05-23 09:58:01 +0700

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -17,9 +17,9 @@ const DashboardPage = () => {
     const fetchStats = async () => {
       try {
         const [evRes, catRes, spkRes] = await Promise.all([
-          api.get('/events'),
-          api.get('/categories'),
-          api.get('/pembicara'),
+          api.get('api/events'),
+          api.get('api/categories'),
+          api.get('api/pembicara'),
         ]);
         const upcoming = evRes.data.filter((e: { status: string }) => e.status === 'upcoming').length;
         setStats({
